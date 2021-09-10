2a6fc5db67324d1a559f5f971c05790f9b37e2bd
pestrinmarco <pestrinmarco@gmail.com>
2021-09-10 13:29:30 -0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# rxjs-boilerplate
+# Description
+Example about a data stream by Observable with RXJS library
+
+## Author
+Marco Pestrin
+
+## Documentation
+https://rxjs.dev/
---
+++ index.js
@@ -0,0 +1,36 @@
+import { fromEvent, Observable } from "rxjs";
+import { debounceTime, mergeMap, filter, map, distinctUntilChanged } from "rxjs/operators";
+import { ajax } from "rxjs/ajax";
+
+function fromEvent( el: HTMLElement, eventType: string) {
+    return new Observable(observer => {
+        const fn = (event: KeyboardEvent) => observer.next(event);
+        el.addEventListener(eventType, fn);
+        return () => el.removeEventListener(eventType, fn);
+    })
+}
+
+const METEO = "https://api.openweathermap.org/data/2.5/weather";
+const TOKEN = "4816db2bc1d7d5621ccadd87644bd0a6";
+
+const sub = fromEvent(document.getElementById("myInput"), "input")
+    .pipe(
+        map((ev) => (ev.target as HTMLInputElement).value),
+        filter((text) => text.length > 3),
+        debounceTime(1000),
+        distinctUntilChanged(),
+        mergeMap((text) => {
+            ajax.get(`${METEO}?q=${text}&units=metric&APPID=${TOKEN}`)
+                .subscribe((res) => {
+                    console.log("res", res.response);
+                })
+        }),
+        map((res) => res.response.main.temp)
+    )
+    .subscribe((temp) => {
+        console.log("temp", temp);
+    });
+
+setTimeout(() => {
+    sub.unsubscribe();
+}, 3000)
